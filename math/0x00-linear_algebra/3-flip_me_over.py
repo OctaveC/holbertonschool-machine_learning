4a43306d02418f5364521f893b3337ac6d779976
OctaveC <octave.collombel@gmail.com>
2022-04-14 21:50:59 +0000

    pycodestyle

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -9,7 +9,7 @@ def matrix_transpose(matrix):
     Returns transpose of a 2D matrix
     """
     trans = []
-    
+
     for row in range(len(matrix[0])):
         trans.append([])
         for col in range(len(matrix)):
